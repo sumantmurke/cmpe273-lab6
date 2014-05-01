ae2982d37e75286b6be07a5581c2c090eb4d08b7
sumant murke <sumantmurke@gmail.com>
2014-04-30 19:17:57 -0700

    final commit

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -12,18 +12,7 @@ public class Client {
     	
     	char[] Mappedvalues = {'x', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
         System.out.println("Starting Cache Client...");
-    /*  
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
-
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-
-*/        
-       
+  
         List<CacheServiceInterface> AvailableServers = new ArrayList<CacheServiceInterface>();
         //adding server to the list
         AvailableServers.add(new DistributedCacheService("http://localhost:3000"));
@@ -31,24 +20,28 @@ public class Client {
         AvailableServers.add(new DistributedCacheService("http://localhost:3002"));
         
         
-        
+        /*
+         *  Function for putting key and values for sharding
+         */
         
           System.out.println("Sharding of key => value onto the servers");
           System.out.println("");
           for(int i=1; i<=10; i++)	{
           	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i)), AvailableServers.size());
           	AvailableServers.get(bucket).put(i, Character.toString(Mappedvalues[i]));
-          	System.out.println("Key "+i+" with value "+Mappedvalues[i]+""
-          			+ " is shaeded over localhost@300" +bucket);
+          	System.out.println("Key "+i+" with value "+Mappedvalues[i]+""+ " is sharded over localhost@300" +bucket);
           }
+          
+          /*
+           *  Function for getting key and values from the respective servers
+           */
           
           System.out.println("");
           System.out.println("Getting keys => values from the servers");
           System.out.println("");
           for(int j=1; j<=10; j++)	{
           	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(j)), AvailableServers.size());
-          	System.out.println("Key " +j+ " with Value "+AvailableServers.get(bucket).get(j)
-          			+ " is fetched from the server: localhost@300" +bucket);      			
+          	System.out.println("Key " +j+ " with Value "+AvailableServers.get(bucket).get(j)+ " is fetched from the server: localhost@300" +bucket);      			
           }
         
         
